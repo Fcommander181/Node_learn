7b9e5edc642424a1b88e3c4c834e149939e3105a
Fcommander181 <commandercat181@gmail.com>
2024-02-08 21:53:03 +0100

    Tutorial Accomplished

--- app.js
+++ app.js
@@ -1,33 +1,97 @@
+// connect to database and import related files and libraries
+const mongoose = require('mongoose');
+const blogRoutes = require("./routes/blogRoutes");
+const dbURI = 'mongodb+srv://Node_Commander:m1n9g6a3@cluster.m4j1ucc.mongodb.net/node_tuto';
+mongoose.connect(dbURI)
+.then(() => app.listen(3000))
+.catch((err) => console.log(err));
 // 1- import express
 const express = require('express');
 // 2- set up express app
 // usually people name this file and the express app as "app"
 const app = express();
 
+
+// middleware & static files
+// require a 3rd party middleware
+app.use(express.static('public')); /* we are setting up out static files in a folder named public */
+const morgan = require('morgan');/*middelware logger */
+app.use(express.urlencoded({extended : true})); /* takes all the url encoded data and pass that into an object we can use on the request object */
+// add a middleware
+/* 
+app.use((req,res,next) => {
+    console.log('new request made');
+    console.log('host: ',req.hostname);
+    console.log('path: ',req.path);
+    console.log('method: ',req.method);
+    next();
+});*/
+// after executing the middleware above , the browser still loading and keeps the old page that's because express doesn't know what to do next. 
+
+// use 3rd party middleware
+
+app.use(morgan('dev'));
+
+
+/*
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req,res) => {
+    const blog = new Blog({
+        title : 'new blog 2',
+        snippet : 'about new blog' , 
+        body : 'more about my new blog'
+    });
+    blog.save()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+
+app.get('/all-blogs', (req,res) => {
+    Blog.find()
+        .then((result) =>  {
+            res.send(result);
+        })
+        .catch((err)=> {
+            console.log(err);
+        })
+});
+
+app.get('/single-blog',(req,res) => {
+    Blog.findById('ID')
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        
+    })
+    */
 // register view engine for this app
 app.set('view engine' , 'ejs');/* we use it to configure some application settings */
 // set the views to another folder (if we didnt do that the default is set to views folder)
 //app.set('views','myviews');
 
 // 3- listen to requests
-app.listen(3000);
+
 //  4- respond to requests
 app.get('/'/* url path */,(req,res) => {
-    const blogs = 
-    {title : "yoshi finds eggs", snippet: 'lorem ipsum dodlor sit amet consecteur'},
-    {title : "mario finds stars",snippet: 'lorem ipsum dodlor sit amet consecteur'},
-    {title : "How to defeat broewser" eggs',snippet: 'lorem ipsum dodlor sit amet consecteur'}
     
     // send response
     // res.send('<p> home page </p>');/* it set the content type header and status code automatically (in most cases)*/ put a string in parameter
     //res.sendFile('./views/index.html',{root: __dirname} /* set the relative path because the default path of sendFile is absolute*/);/* it set the content type header and status code automatically (in most cases)*/
-    res.render('index',/*to pass data from our handler to a view*/{ title : 'Home'}); // res.render generates HTML on the server and delivers it to the client.
+    //res.render('index',/*to pass data from our handler to a view*/{ title : 'Home' , blogs}); // res.render generates HTML on the server and delivers it to the client.
+    res.redirect('/blogs');
 });
     
+// blog routes
+app.use('/blogs',blogRoutes);
 
-app.get('/blogs/create' , (req,res) => {
-    res.render('create', {title : 'Create a new Blog'});
-});
+
 
 app.get('/about'/* url path */,(req,res) => {
     // send response
@@ -48,3 +112,5 @@ app.use((req,res) => {
 }); // use method callback a function  for every single request coming in but only when the request reaches this point in the code (like the default in switch) it should go in the bottom of the page
 
 // express works in the way that if a request match with url and handlers (like get) it will send response and ignore the code after else it will match the next gets handlers and url 
+
+
